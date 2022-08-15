c12200fdbd8ad19f38ceb0464a82b83c8664e0b9
Syhofrukt <92628946+Syhofrukt@users.noreply.github.com>
2022-08-15 22:07:58 +0300

    15.08.22

--- core/start.py
+++ core/start.py
@@ -1,4 +1,5 @@
 import discord
+import asyncio
 
 client = discord.Client()
 
@@ -6,12 +7,57 @@ client = discord.Client()
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+
+player_list = []
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
+    channel = message.channel
+
     if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+        await channel.send('Hello!')
+
+@client.event
+async def on_message(message):
+    members = message.guild.members
+    channel = message.channel
+
+    if message.content == '!pingpong':
+        sluzhebnoe_message = channel.send('{}, вы начали набор в игру Пинг Понг. Чтобы присоединится к игре,'
+                                          ' противник должен написать !game с упоминанием '
+                                          'игрока запросившего поиск игры (через пробел). Также вы можете написать '
+                                          '!game с упоминанием игрока (через пробел), с которым вы хотите сыграть'
+                                          .format(message.author.mention), reference=message)
+
+        await sluzhebnoe_message
+
+        def check(message1):
+            if message1.content.startswith('!game ' + message.author.mention) and message1.author != message.author:
+                player_list.append(message.author)
+                player_list.append(message1.author)
+
+
+            for member in members:
+                if member != message1.author:
+                    if message1.content.startswith('!game ' + member.mention):
+                        player_list.append(message1.author)
+                        player_list.append(member)
+
+            return message1.reference == sluzhebnoe_message
+
+        while True:
+            try:
+                reply = await client.wait_for('message', timeout=120.0, check=check)
+            except asyncio.TimeoutError:
+                await channel.send('{}, время набора истекло'.format(message.author.mention), reference=message)
+                break
+
+            break
+
+        await channel.send('{}, вы играете против игрока {}'.format(player_list[0].mention, player_list[1].mention))
+
 
 client.run('MTAwODQzMDE0NzcyMjAyNzAwOA.G5lphK.nU5ETEbRY4AxnwiCWIegBfCCzQiHYJVmTCh6Lk')
